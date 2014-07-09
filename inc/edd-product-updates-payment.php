0cc5c5dfa258dda81fcfac9b74e723902422b488
Evan <evan@theblackandblue.com>
2014-07-09 12:39:19 -0400

    Formatting and docblock

--- inc/edd-product-updates-payment.php
+++ inc/edd-product-updates-payment.php
@@ -1,15 +1,26 @@
 <?php
-/*
-Description: Send product update emails in batch
-Version: 0.1
-Author: Evan Luzi
-Author URI: http://evanluzi.com
-Contributors: Evan Luzi
-*/
+/**
+ * EDD Product Updates Payment History
+ *
+ * Add unsubscribe option on payment history page for customers
+ *
+ *
+ * @package    EDD_PUP
+ * @author     Evan Luzi
+ * @copyright  Copyright 2014 Evan Luzi, The Black and Blue, LLC
+ * @since      0.9
+ */
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Add the "Send Product Updates?" option to payment history page
+ * 
+ * @access public
+ * @param mixed $payment_id
+ * @return void
+ */
 function edd_pup_order_history($payment_id){
 
 	$payment_meta = edd_get_payment_meta($payment_id);
@@ -19,9 +30,9 @@ function edd_pup_order_history($payment_id){
 	?>
 			<div class="edd-admin-box-inside edd-send-updates">
 				<p>
-					<span class="label" title="<?php _e( 'Grants the customer unlimited file downloads for this purchase, regardless of other limits set.', 'edd' ); ?>"><i data-code="f463" class="dashicons dashicons-update"></i></span>&nbsp;
+					<span class="label" title="<?php _e( 'When checked, customer will receive product update emails.', 'edd_pup' ); ?>"><i data-code="f463" class="dashicons dashicons-update"></i></span>&nbsp;
 					<input type="checkbox" name="edd-send-product-updates" id="edd_send_product_updates" value="1"<?php checked( true, $sendupdates, true ); ?>/>
-					<label class="description" for="edd_send_product_updates"><?php _e( 'Send Product Updates', 'edd' ); ?></label>
+					<label class="description" for="edd_send_product_updates"><?php _e( 'Send Product Updates', 'edd_pup' ); ?></label>
 				</p>
 			</div>
 			<?php
@@ -30,9 +41,12 @@ function edd_pup_order_history($payment_id){
 }
 add_action('edd_view_order_details_update_inner','edd_pup_order_history');
 
-
 /**
- * Store the custom field data into EDD's payment meta
+ * Store the unsubscribe custom field into EDD's payment meta
+ * 
+ * @access public
+ * @param mixed $payment_meta
+ * @return void
  */
 function edd_pup_store_field( $payment_meta ) {
 
@@ -41,9 +55,13 @@ function edd_pup_store_field( $payment_meta ) {
     return $payment_meta;
 }
 add_filter( 'edd_payment_meta', 'edd_pup_store_field');
-
+ 
 /**
- * Save the phone field when it's modified via view order details
+ * Save unsubscribe field when modified on payment history page
+ * 
+ * @access public
+ * @param mixed $payment_id
+ * @return void
  */
 function edd_pup_updated_edited_purchase( $payment_id ) {
  
@@ -59,11 +77,11 @@ function edd_pup_updated_edited_purchase( $payment_id ) {
 add_action( 'edd_updated_edited_purchase', 'edd_pup_updated_edited_purchase' );
 
 /**
- * edd_pup_user_send_updates function.
+ * Returns boolean whether or not customer is subscribed for updates
  * 
  * @access public
  * @param mixed $payment_id
- * @return void
+ * @return bool $sendupdates 
  */
 function edd_pup_user_send_updates($payment_id){
     
