c7099417c9b328290e92c58778d2faaf4515d3c7
Evan <evan@theblackandblue.com>
2014-10-04 11:42:08 -0400

    Add alerts for emails in queue and in processing. Add logic for displaying processed and queued emails on those emails in queue.

--- inc/view-pup-email.php
+++ inc/view-pup-email.php
@@ -17,6 +17,21 @@ $email     = get_post( $email_id );
 $emailmeta = get_post_custom( $email_id );
 $updated_products = get_post_meta( $email_id, '_edd_pup_updated_products', TRUE );
 $queue = edd_pup_check_queue( $email_id );
+$processing = true;
+
+switch ( strtolower( $email->post_status ) ){
+		case 'publish':
+			$status = __( 'Sent', 'edd-pup' );
+			break;
+		case 'pending':
+			$status = __( 'In Queue', 'edd-pup' );
+			break;
+		case 'abandoned':
+			$status = __( 'Cancelled', 'edd-pup' );
+			break;
+		default:
+			$status = ucfirst ( $email->post_status );
+}
 
 ?>
 <div id="edd-pup-admin-email" class="wrap">
@@ -28,8 +43,12 @@ $queue = edd_pup_check_queue( $email_id );
 					<div class="postbox">
 						<h3 class="hndle"><span><?php _e( 'Product Update Email Info', 'edd-pup' ); ?></span></h3>
 						<div class="inside">
-							<p><strong><?php _e( 'Status', 'edd-pup' ); ?>:</strong> <span class="status-publish">Sent</span></p>
-							<p><strong><?php _e( 'Recipients', 'edd-pup' ); ?>:</strong> <?php echo $emailmeta['_edd_pup_recipients'][0]; ?></p>
+							<p><strong><?php _e( 'Status', 'edd-pup' ); ?>:</strong> <span class="status-<?php echo $email->post_status; ?>"><?php echo $status; ?></span></p>							
+							<?php if ( $queue['queue'] > 0 ): ?>
+							<p><strong><?php _e( 'Queued', 'edd-pup' ); ?>:</strong> <?php echo $queue['queue']; ?></p>
+							<p><strong><?php _e( 'Processed', 'edd-pup' ); ?>:</strong> <?php echo $queue['sent']; ?></p>
+							<?php endif; ?>
+							<p><strong><?php _e( 'Total Recipients', 'edd-pup' ); ?>:</strong> <?php echo $emailmeta['_edd_pup_recipients'][0]; ?></p>
 							<p><strong><?php _e( 'Updated Products', 'edd-pup' ); ?>:</strong></p>
 								<ul id="updated-products">
 								<?php foreach ( $updated_products as $id => $title ): ?>
@@ -40,18 +59,36 @@ $queue = edd_pup_check_queue( $email_id );
 					</div>
 				</div>	
 			</div>
+
 			<?php if ( $queue['queue'] > 0 ) : ?>
 			<div id="postbox-container-2" class="postbox-container edd-pup-view-alert">
 				<div id="normal-sortables" class="meta-box-sortables ui-sortable">
 					<div class="postbox">
-						<h3 class="hndle"><span><?php _e( 'Here is the Alert Title', 'edd-pup' ); ?></span></h3>
+						<h3 class="hndle"><span><?php _e( 'Action Needed – Email in Send Queue', 'edd-pup' ); ?></span></h3>
 						<div class="inside">
-							<p>This would be the alert message</p>
+							<p><?php _e( 'This email you are viewing still has messages to send and is currently not processing. Please choose to either:', 'edd-pup' ); ?></p>
+							<p>1. <?php printf( __( '<a href="%s">Resume sending this email</a> to the remaining customers in the queue.', 'edd-pup' ), '#' ); ?></p>
+							<p>2. <?php printf( __( '<a href="%s" class="%s" data-action="%s" data-email="%s">Clear the email from the queue</a> and cancel sending this email to the customers who have not received it.', 'edd-pup' ), '#', 'edd-pup-queue-button', 'edd_pup_clear_queue', $email_id ); ?></p>
+							<p><strong><?php _e( 'If no action is taken within 48 hours, this email will be automatically removed from the queue.', 'edd-pup' ); ?></strong></p>
 						</div>
 					</div>
 				</div>
 			</div>
 			<?php endif; ?>
+			
+			<?php if ( $processing ) : ?>
+			<div id="postbox-container-2" class="postbox-container edd-pup-view-processing">
+				<div id="normal-sortables" class="meta-box-sortables ui-sortable">
+					<div class="postbox">
+						<h3 class="hndle"><span><?php _e( 'Email Currently Processing', 'edd-pup' ); ?></span></h3>
+						<div class="inside">
+							<p><?php _e( 'This email is currently processing and being used to send messages to customers of the products listed on the sidebar.', 'edd-pup' ); ?></p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php endif; ?>
+			
 			<div id="postbox-container-3" class="postbox-container">
 				<div id="normal-sortables" class="meta-box-sortables ui-sortable">
 					<div class="postbox">
