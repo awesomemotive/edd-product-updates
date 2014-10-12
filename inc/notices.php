92b8c168b0f9e99a3da952828744766842adafb8
Evan <apple@apples-mbp.home>
2014-10-12 01:22:25 -0400

    Bugfix for php warnings being showed on non-product-updates pages from notices.

--- inc/notices.php
+++ inc/notices.php
@@ -18,36 +18,35 @@ function edd_pup_notices() {
 	$style = 'updated';
 	$supporturl = 'https://easydigitaldownloads.com/support/';
 	
-	if ( $_GET['post_type'] != 'download' && $_GET['page'] != 'edd-prod-updates' ) {
-		return;
-	}
-	
-	if ( isset( $_GET['edd_pup_notice'] ) ) {
-		
-		switch ( $_GET['edd_pup_notice'] ) {
-			case 1:
-				$message = __( 'Product Update Email changes saved successfully.', 'edd-pup' );
-				break;
-			case 2:
-				$message = __( 'Product Update Email saved successfully.', 'edd-pup' );
-				break;
-			case 3:
-				$message = sprintf( __( 'Product Update Email changes <strong>did not save successfully</strong>. If the issue continues, please <a href="%s" target="_%s">contact Easy Digital Downloads support</a> for help.', 'edd-pup' ), $supporturl, '_blank' );
-				$style = 'error';
-				break;
-			case 4:
-				$message = sprintf( __( 'Email was <strong>not deleted successfully</strong>. If the issue continues, please <a href="%s" target="_%s">contact Easy Digital Downloads support</a> for help.', 'edd-pup' ), $supporturl, '_blank' );
-				$style = 'error';
-				break;
-			case 5:
-				$message = __( '1 email successfully deleted.', 'edd-pup' );
-				break;
-		}
-    ?>
-    <div class="<?php echo $style; ?> edd-pup-message">
-        <p><?php echo $message; ?></p>
-    </div>
-    <?php
+	if ( isset( $_GET['post_type'] ) && ( $_GET['post_type'] == 'download' ) && ( $_GET['page'] == 'edd-prod-updates' ) ) {
+
+		if ( isset( $_GET['edd_pup_notice'] ) ) {
+			
+			switch ( $_GET['edd_pup_notice'] ) {
+				case 1:
+					$message = __( 'Product Update Email changes saved successfully.', 'edd-pup' );
+					break;
+				case 2:
+					$message = __( 'Product Update Email saved successfully.', 'edd-pup' );
+					break;
+				case 3:
+					$message = sprintf( __( 'Product Update Email changes <strong>did not save successfully</strong>. If the issue continues, please <a href="%s" target="_%s">contact Easy Digital Downloads support</a> for help.', 'edd-pup' ), $supporturl, '_blank' );
+					$style = 'error';
+					break;
+				case 4:
+					$message = sprintf( __( 'Email was <strong>not deleted successfully</strong>. If the issue continues, please <a href="%s" target="_%s">contact Easy Digital Downloads support</a> for help.', 'edd-pup' ), $supporturl, '_blank' );
+					$style = 'error';
+					break;
+				case 5:
+					$message = __( '1 email successfully deleted.', 'edd-pup' );
+					break;
+			}
+	    ?>
+	    <div class="<?php echo $style; ?> edd-pup-message">
+	        <p><?php echo $message; ?></p>
+	    </div>
+	    <?php
+	    }
     }
 }
 add_action( 'admin_notices', 'edd_pup_notices', 10);
